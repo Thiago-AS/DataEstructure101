10771ede9f8de97e55f2915f439046bcfaf3d799
Thiago Araujo <thiagos.araujo09@gmail.com>
2018-05-22 11:47:34 -0300

    Basic stack structure and construction functions

--- Calculadora/stack.c
+++ Calculadora/stack.c
@@ -1,2 +1,30 @@
 #include "stack.h"
 
+header* CreateStack(){
+    header* sHead = (header*) malloc(sizeof(header));
+    sHead->stack = NULL;
+    sHead->amount = 0;
+    return sHead;
+}
+
+void PushChar(header* sHead, char value){
+    stack *element = (stack*) malloc(sizeof(stack));
+    element->value = value;
+    element->next = sHead->stack;
+    sHead->stack = element;
+    sHead->amount++;
+}
+
+char PopChar(header *sHead){
+    char character;
+    if(sHead->amount != 0){
+        sHead->amount--;
+        character = sHead ->stack->value;
+        free(sHead->stack);
+        sHead->stack = sHead->stack->next;
+        return character;
+    }
+    else{
+        return '\0';
+    }
+}
--- Calculadora/stack.h
+++ Calculadora/stack.h
@@ -5,6 +5,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct stack{
+    unsigned int value;
+    struct stack *next;
+}stack;
+
+typedef struct header{
+    struct stack* stack;
+    int amount;
+}header;
+
+header* CreateStack();
+
+void PushChar(header* pHead, char value);
+
+char PopChar(header *pHead);
+
 
 
 #endif //CALCULADORA_STACK_H
